22908da33dd398f57eb7f8fb182705c39c8fd8de
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-10-24 11:48:40 +0200

    Add project files.

---
+++ CoreCodeCamp/Controllers/CampsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    public class CampsController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            if (false) return BadRequest("Bad stuff happened");
+            return Ok(new { Moniker = "ATL2018", Name = "Atlanta Code Camp"});
+        }
+    }
+}
