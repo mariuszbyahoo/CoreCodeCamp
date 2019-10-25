3ddb28708e6824e37ea210844329e0787648cfba
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-10-25 09:50:08 +0200

    Module 5 Create an Association Controller

---
+++ CoreCodeCamp/Controllers/TalksController.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using CoreCodeCamp.Data;
+using CoreCodeCamp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [ApiController]
+    [Route("api/camps/{moniker}/talks")]
+    public class TalksController : ControllerBase
+    {
+        private readonly ICampRepository repository;
+        private readonly IMapper mapper;
+        private readonly LinkGenerator linkGenerator;
+
+        public TalksController(ICampRepository repository, IMapper mapper, LinkGenerator linkGenerator)
+        {
+            this.repository = repository;
+            this.mapper = mapper;
+            this.linkGenerator = linkGenerator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<TalkModel[]>> Get(string moniker)
+        {
+            try
+            {
+                var talks = repository.GetTalksByMonikerAsync(moniker);
+                var mappedTalks = mapper.Map<TalkModel[]>(talks.Result);
+                return mappedTalks;
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Failed to get talks");
+            }
+        }
+    }
+}
