a1222c30af36f6dde71977ee7158508740091cc5
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-11-01 19:50:31 +0100

    Versioning Your API: Introducing Versioning

--- CoreCodeCamp/Startup.cs
+++ CoreCodeCamp/Startup.cs
@@ -31,10 +31,15 @@ namespace CoreCodeCamp
             services.AddDbContext<CampContext>();
             services.AddScoped<ICampRepository, CampRepository>();
 
-            services.AddMvc()
+            services.AddMvc(opt => opt.EnableEndpointRouting = false)
                 .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
 
             services.AddAutoMapper();
+            services.AddApiVersioning(opt =>
+            {
+                opt.DefaultApiVersion = new ApiVersion(1, 1);
+                opt.ReportApiVersions = true;
+            });
 
             services.AddControllers();
         }
